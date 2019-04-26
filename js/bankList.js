d6cadf5db0d4a4d80985b6016bee37b7988c6986
windsailg <38423109+windsailg@users.noreply.github.com>
2019-04-26 13:53:26 +0800

    Add files via upload

--- js/bankList.js
+++ js/bankList.js
@@ -8,10 +8,9 @@ $('.footer').load('footer.html');
 $(document).ready(function(){
 
 
-
+//charts
 	google.charts.load("current", {packages:['corechart']});
 	google.charts.setOnLoadCallback(drawChart);
-
 	function drawChart() {
 	  var data = google.visualization.arrayToDataTable([
 			['Element', '年薪', { role: 'style' }],
@@ -64,18 +63,31 @@ $(document).ready(function(){
 	  
 	  var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
 	  chart.draw(view, options);
- }
+	}
 
 
 
 
+//Aos init
+AOS.init({
+	// Global settings:
+	disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
 
+	useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+	disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+	debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+	throttleDelay: 100, // the delay on throttle used while scrolling the page (advanced)
 
+	// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+	offset: 120, // offset (in px) from the original trigger point
+	delay: 0, // values from 0 to 3000, with step 50ms
+	duration: 800, // values from 0 to 3000, with step 50ms
+	easing: 'ease-in-out-back', // default easing for AOS animations
+	once: false, // whether animation should happen only once - while scrolling down
+	mirror: true, // whether elements should animate out while scrolling past them
+	anchorPlacement: 'top-bottom', // 視窗觸發位置
 
-
-	
-
-
+});
 
 		$('#Chen').click(function(){
 			$('#Chen').toggleClass('active');
